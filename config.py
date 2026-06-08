47434981d1a96e53d5d03e12369572b6a32fb49d
nhdata0-star <nhdata0@gmail.com>
2026-06-08 09:50:21 +0600

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import pytz
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+#import pytz
+#from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-scheduler = AsyncIOScheduler(timezone=pytz.UTC)  #pytz.timezone("Asia/Phnom_Penh")
+#scheduler = AsyncIOScheduler(timezone=pytz.UTC)  #pytz.timezone("Asia/Phnom_Penh")
 
 PORT = 5000
 TELEGRAM_BOT_TOKEN = '7931517263:AAHTHHlaDNopwnvU1spYtlNahUH3FW2yO04'
